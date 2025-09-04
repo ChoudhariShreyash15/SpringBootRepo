5d149f2bbfe59099d188a8a4551493dbdd58e570
ChoudhariShreyash15 <shreyashchoudhari02@gmail.com>
2025-09-04 18:03:40 +0530

    first

--- SpringBeans/src/main/java/org/example/App.java
+++ SpringBeans/src/main/java/org/example/App.java
@@ -10,10 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        AnnotationConfigApplicationContext ap = new AnnotationConfigApplicationContext(Config.class);
+        ApplicationContext ap = new AnnotationConfigApplicationContext(Config.class);
         Vehicle v = ap.getBean(Vehicle.class);
         v.engineType();
-        ap.close();
 
 //        ApplicationContext applicationContext=new ClassPathXmlApplicationContext("Config.xml");
 //        Vehicle vehicle=applicationContext.getBean(Vehicle.class);
